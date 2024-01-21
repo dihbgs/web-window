7e82d3313ff7057efa610c722c2a672127885844
dihbgs <dihborgesdr@gmail.com>
2024-01-21 18:43:17 -0300

    feat: add controller code for updating user

--- src/interfaces/user/controller.ts
+++ src/interfaces/user/controller.ts
@@ -2,6 +2,12 @@ import { User, UserDTO } from "../../models/user";
 import { HttpRequest, HttpResponse } from "../common/http";
 
 export interface IUserController {
+  updateOne(
+    id: string,
+    newUser: HttpRequest<UserDTO>
+  ): Promise<HttpResponse<UserDTO>>;
+  deleteAll(): Promise<HttpResponse<UserDTO[]>>;
+  deleteOne(id: string): Promise<HttpResponse<UserDTO>>;
   createOne(newUser: HttpRequest<UserDTO>): Promise<HttpResponse<UserDTO>>;
   getAll(): Promise<HttpResponse<User[]>>;
 }

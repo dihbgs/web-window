c5ddaa50d234d82f0683b0108073154257edd3c2
dihbgs <dihborgesdr@gmail.com>
2024-01-21 18:11:24 -0300

    feat: add update user

--- src/interfaces/user/repository.ts
+++ src/interfaces/user/repository.ts
@@ -3,6 +3,7 @@ import { User, UserDTO } from "../../models/user";
 export type UserDB = Omit<User, "id">;
 
 export interface IUserRepository {
+  updateOne(id: string, newUser: UserDTO): Promise<UserDTO>;
   createOne(newUser: UserDTO): Promise<UserDTO>;
   deleteOne(id: string): Promise<UserDTO>;
   deleteAll(): Promise<UserDTO[]>;

094d6a14aedb9dc5fd59d8e5d10128bcabaaa0d8
dihbgs <dihborgesdr@gmail.com>
2024-01-21 22:17:53 -0300

    feat: add some test cases

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-import { OK, CREATED } from "../src/interfaces/common/http";
+import { STATUS } from "../src/interfaces/common/http";
 import chaiHttp from "chai-http";
 import { expect } from "chai";
 import chai from "chai";
@@ -7,14 +7,27 @@ const server = "http://localhost:8000";
 
 chai.use(chaiHttp);
 
-describe("Get all users", () => {
-  it("should be able to return all the users", (done) => {
+const user = {
+  email: "user@test.new",
+  username: "testUser",
+  password: "password",
+  id: undefined,
+};
+
+const newUser = {
+  id: user.id,
+  email: "user@new.name",
+  username: "noMoreATestUser",
+  password: "justKidding",
+};
+
+describe("Clear database", () => {
+  it("should be able to clear database", (done) => {
     chai
       .request(server)
-      .get("/users")
+      .delete("/users")
       .end((err, res) => {
-        expect(res).to.have.status(OK);
-        expect(res.body).to.be.an("array");
+        expect(res).to.have.status(STATUS.OK);
         done();
       });
   });
@@ -25,13 +38,23 @@ describe("Create a user", () => {
     chai
       .request(server)
       .post("/users")
-      .send({
-        email: "new@user.com",
-        username: "testUser",
-        password: "665544",
-      })
+      .send(user)
       .end((err, res) => {
-        expect(res).to.have.status(CREATED);
+        expect(res).to.have.status(STATUS.CREATED);
+        done();
+      });
+  });
+});
+
+describe("Get all users", () => {
+  it("should be able to return all the users", (done) => {
+    chai
+      .request(server)
+      .get("/users")
+      .end((err, res) => {
+        expect(res).to.have.status(STATUS.OK);
+        expect(res.body).to.be.an("array");
+        expect(res.body.length).to.be.equal(1);
         done();
       });
   });
@@ -49,6 +72,63 @@ describe("Insert duplicated user", () => {
       })
       .end((err, res) => {
         expect(res).to.have.status(400);
+        done();
+      });
+  });
+});
+
+describe("Get a user by username", () => {
+  it("should be able to return a user by username", (done) => {
+    chai
+      .request(server)
+      .get(`/users/${user.username}`)
+      .end((err, res) => {
+        expect(res).to.have.status(STATUS.OK);
+        expect(res.body).to.be.an("object");
+        expect(res.body.username).to.be.equal("testUser");
+        expect(res.body.id).to.not.be.undefined;
+        user.id = res.body.id;
+        done();
+      });
+  });
+});
+
+describe("Update a user", () => {
+  it("should be able to update a user", (done) => {
+    chai
+      .request(server)
+      .put(`/users/${user.username}`)
+      .send(newUser)
+      .end((err, res) => {
+        expect(res).to.have.status(STATUS.OK);
+        expect(res.body).to.be.an("object");
+        expect(res.body.username).to.be.equal("noMoreATestUser");
+        done();
+      });
+  });
+});
+
+describe("Get a user by id", () => {
+  it("should be able to return a user by id", (done) => {
+    chai
+      .request(server)
+      .get(`/users/${user.id}`)
+      .end((err, res) => {
+        expect(res).to.have.status(STATUS.OK);
+        expect(res.body).to.be.an("object");
+        expect(res.body.username).to.be.equal("noMoreATestUser");
+        done();
+      });
+  });
+});
+
+describe("Delete a user", () => {
+  it("should be able to delete a user", (done) => {
+    chai
+      .request(server)
+      .delete(`/users/${user.id}`)
+      .end((err, res) => {
+        expect(res).to.have.status(STATUS.OK);
         done();
       });
   });

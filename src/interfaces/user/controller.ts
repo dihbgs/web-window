574944139b07f2414e3ed26fa83c8a23cd699775
dihbgs <dihborgesdr@gmail.com>
2024-01-23 14:12:53 -0300

    feat: modify controller  interface

--- src/interfaces/user/controller.ts
+++ src/interfaces/user/controller.ts
@@ -1,16 +1,16 @@
 import { HttpRequest, HttpResponse } from "../common/http";
 import { UserDTO } from "../../models/user";
 
+export type UserUpdateRequest = HttpRequest<{ oldUsername: string } & UserDTO>;
 export type UserArrayResponse = HttpResponse<UserDTO[]>;
 export type UserResponse = HttpResponse<UserDTO>;
 export type UserRequest = HttpRequest<UserDTO>;
 
 export interface IUserController {
-  updateOne(request: UserRequest): Promise<UserResponse>;
+  updateOne(request: UserUpdateRequest): Promise<UserResponse>;
   createOne(request: UserRequest): Promise<UserResponse>;
-  getByName(request: UserRequest): Promise<UserResponse>;
+  getOne(request: UserRequest): Promise<UserResponse>;
   deleteOne(request: UserRequest): Promise<UserResponse>;
-  getById(request: UserRequest): Promise<UserResponse>;
   deleteAll(): Promise<UserArrayResponse>;
   getAll(): Promise<UserArrayResponse>;
 }

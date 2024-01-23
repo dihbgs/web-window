ca19f8e954caf92c9da9e942f4651307306bf971
dihbgs <dihborgesdr@gmail.com>
2024-01-23 14:12:37 -0300

    feat: modify repository interface

--- src/interfaces/user/repository.ts
+++ src/interfaces/user/repository.ts
@@ -1,15 +1,15 @@
 import { User, UserDTO } from "../../models/user";
 
+export type UserUpdate = { oldUsername: string } & UserDTO;
 export type ResponseDTOArray = Promise<UserDTO[]>;
 export type ResponseDTO = Promise<UserDTO>;
 export type UserDB = Omit<User, "id">;
 
 export interface IUserRepository {
-  updateOne(user: UserDTO): ResponseDTO;
+  updateOne(user: UserUpdate): ResponseDTO;
   createOne(user: UserDTO): ResponseDTO;
-  getByName(user: UserDTO): ResponseDTO;
   deleteOne(user: UserDTO): ResponseDTO;
-  getById(user: UserDTO): ResponseDTO;
+  getOne(user: UserDTO): ResponseDTO;
   deleteAll(): ResponseDTOArray;
   getAll(): ResponseDTOArray;
 }
